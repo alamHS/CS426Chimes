7fe5f11c166e8241c89f30a050c789c576c91698
Hibah Alam <33077568+alamHS@users.noreply.github.com>
2017-10-26 12:48:10 -0500

    update

---
+++ Beginning/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float playerdistance;
+    public float enemylookdistance;
+    public float attackdistance;
+    public float emenyspeed;
+    public float damping;
+    public Transform target;
+    Rigidbody therigidbody;
+    Renderer myrender;
+	// Use this for initialization
+	void Start () {
+        myrender = GetComponent<Renderer>();
+        therigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        playerdistance = Vector3.Distance(target.position, transform.position);
+        if(playerdistance<enemylookdistance)
+        {
+            myrender.material.color = Color.yellow;
+            look();
+
+        }
+        if (playerdistance < attackdistance)
+        {
+            myrender.material.color = Color.red;
+            attack();
+        }
+        else
+        { myrender.material.color = Color.blue; }
+
+    }
+    void look() {
+        Quaternion rotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation,Time.deltaTime * damping);
+    }
+    void attack() {
+        therigidbody.AddForce(transform.forward * emenyspeed);
+            }
+}
--- Beginning/Assets/Scripts/move.cs
+++ Beginning/Assets/Scripts/move.cs
@@ -8,8 +8,7 @@ public class move : MonoBehaviour {
 	public float speed = 25.0f;
 	public float rotationSpeed = 90;
 	public float force = 700f;
-	public GameObject cannon;
-	public GameObject bullet;
+
 
 	Rigidbody rb;
 	Transform t;
@@ -21,17 +20,17 @@ public class move : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.DownArrow))
-			rb.velocity += this.transform.forward * speed * Time.deltaTime;
-		else if (Input.GetKey (KeyCode.UpArrow))
+		if (Input.GetKey (KeyCode.UpArrow))
 			rb.velocity -= this.transform.forward * speed * Time.deltaTime;
-		if (Input.GetKey(KeyCode.LeftArrow))
+		else if (Input.GetKey (KeyCode.DownArrow))
+			rb.velocity += this.transform.forward * speed * Time.deltaTime;
+		if (Input.GetKey (KeyCode.RightArrow))
 			t.rotation *= Quaternion.Euler (0, rotationSpeed * Time.deltaTime, 0);
-		else if(Input.GetKey(KeyCode.RightArrow))
+		else if (Input.GetKey (KeyCode.LeftArrow))
 			t.rotation *= Quaternion.Euler (0, -rotationSpeed * Time.deltaTime, 0);
 		if (Input.GetKeyDown (KeyCode.Space))
 			rb.AddForce (t.up * force);
-
+		
 	}
 
 }
