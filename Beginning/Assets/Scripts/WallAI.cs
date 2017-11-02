d8d2a46d75841c0c2254e7359615826707fe583a
Hibah Alam <33077568+alamHS@users.noreply.github.com>
2017-11-02 01:16:31 -0500

    this should work

---
+++ Beginning/Assets/Scripts/WallAI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallAI : MonoBehaviour {
+
+	Rigidbody rumi;
+	Renderer color;
+	// Use this for initialization
+	void Start () {
+		rumi = GetComponent<Rigidbody>();
+		color = GetComponent<Renderer>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
