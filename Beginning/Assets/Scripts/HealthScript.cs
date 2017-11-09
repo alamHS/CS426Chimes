b77ba9b18cec43073486a692e7f0a6d5c56abeec
8vinay <33085311+8vinay@users.noreply.github.com>
2017-11-09 10:59:01 -0600

    added health script

---
+++ Beginning/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour {
+
+	public Slider healthBarSlider;  
+ 	public Text gameOverText;  
+ 	private bool isGameOver = false; 
+ 
+	void Start()
+	{
+  		gameOverText.enabled = false; 
+ 	}
+ 
+
+ 	void Update () 
+	{
+ 	 	
+  		if(!isGameOver)
+   			transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*10f, 0, 0); //get input
+ 	}
+ 
+
+ 	void OnTriggerStay(Collider other)
+	{
+		Debug.Log ("enemy collision");
+  		
+  		if(other.gameObject.name=="enemy1" && healthBarSlider.value>0)
+		{
+   			healthBarSlider.value -=10.0f;  
+  		}
+  		else
+		{
+   			isGameOver = true;    
+   			gameOverText.enabled = true;
+  		}
+ 	}
+}
