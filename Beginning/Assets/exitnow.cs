fbaf7af537f173eede3ed1c7ea60ec231031cc30
Hibah Alam <33077568+alamHS@users.noreply.github.com>
2017-11-16 02:00:49 -0600

    main menu and exit
    done

---
+++ Beginning/Assets/exitnow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitnow : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	public void Update (int one) {
+		Application.Quit ();
+		Debug.Log ("Quit works");
+	}
+}
