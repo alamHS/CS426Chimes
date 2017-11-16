68e015ad0a15ab33c3b676a8d884705d72d67559
Hibah Alam <33077568+alamHS@users.noreply.github.com>
2017-11-16 03:38:31 -0600

    Two riddles am gonna nap now

---
+++ Beginning/Assets/Scripts/blue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blue : MonoBehaviour {
+	public GameObject switch1;
+	public GameObject sign;
+	Renderer color1;
+
+	bool canInteract = false;
+	float displayTime = 3.0f;
+	bool displayMsg = false;
+
+
+	void Start(){
+		color1 = switch1.GetComponent<Renderer>();
+	}
+
+	void Update () {
+
+		if (color1.material.color == Color.green) {
+			displayTime -= Time.deltaTime;
+			if (displayTime <= 0.0)
+				displayMsg = false;
+
+			RaycastHit hit;
+			Ray shootRay = new Ray (this.transform.position, Vector3.back);
+
+			if (Physics.Raycast (shootRay, out hit, 50)) {
+				if (hit.collider.tag == "Player") {
+					canInteract = true;
+					displayMsg = true;
+					displayTime = 3.0f;
+				}
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		
+		if (displayMsg) {
+			if (canInteract) {
+				GUIStyle textStyle = new GUIStyle ();
+				textStyle.normal.textColor = Color.white;
+				GUI.Label (new Rect (500, 100, 300, 20), "From the oceans will you hear rhythms, that is where they will call you", textStyle);
+
+			}
+		}
+	}
+
+}
---
+++ Beginning/Assets/Scripts/red.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class red : MonoBehaviour {
+	bool canInteract = false;
+	float displayTime = 3.0f;
+	bool displayMsg = false;
+
+	void Update () {
+		displayTime -= Time.deltaTime;
+		if (displayTime <= 0.0)
+			displayMsg = false;
+
+		RaycastHit hit;
+		Ray shootRay = new Ray (this.transform.position, Vector3.back);
+
+		if (Physics.Raycast (shootRay, out hit, 50)) {
+			if (hit.collider.tag == "Player") {
+				canInteract = true;
+				displayMsg = true;
+				displayTime = 3.0f;
+			}
+		}
+	}
+
+		void OnGUI()
+		{
+			if (displayMsg) {
+				if (canInteract) {
+					GUIStyle textStyle = new GUIStyle ();
+					textStyle.normal.textColor = Color.white;
+					GUI.Label (new Rect (500, 100, 300, 20), "The wind will extinguish this searing hue", textStyle);
+
+				}
+			}
+		}
+
+}
