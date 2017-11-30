e0fc4196d1e7da77f4fcfbb14eebf162640caf98
aygoyal01 <aygoyal01@github.com>
2017-11-30 05:19:43 -0600

    Update
    Shaders added
    Signs functioning corrected

---
+++ Beginning/Assets/shader/y.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class y : MonoBehaviour {
+
+    // Use this for initialization
+    private GUIStyle guiStyle = new GUIStyle();
+    bool canInteract = false;
+    float displaytime = 3.0f;
+    bool displayMsg = false;
+    public GameObject player;
+    void Update()
+    {
+        displaytime -= Time.deltaTime;
+        if (displaytime <= 0.0)
+            displayMsg = false;
+    }
+        public void OnTriggerStay()
+    {
+        if (player.tag == "Player")
+        {
+            displayMsg = true;
+            canInteract = true;
+            displaytime = 3.0f;
+        }
+    }
+    void OnGUI()
+    {
+        if (displayMsg)
+        {
+            if (canInteract)
+            {
+                
+                guiStyle.fontSize = 30;
+                guiStyle.normal.textColor = Color.red;
+                GUI.Label(new Rect(500, 100, 300, 20), "The wind will extinguish this searing hue",guiStyle);
+
+            }
+        }
+    }
+
+
+}
